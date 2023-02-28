4e247585696d4f3489f4f442606f4c3dbf0f87d5
MH.Xu <Minghao.Xu.1@warwick.ac.uk>
2023-02-28 14:55:51 +0000

    add accum_nwot_ssnr metric

--- zero_cost_metrics/__init__.py
+++ zero_cost_metrics/__init__.py
@@ -4,7 +4,7 @@ from foresight.pruners import predictive
 from zero_cost_metrics.naswot import get_naswot
 from zero_cost_metrics.ntk import get_ntk_n
 from zero_cost_metrics.zen import zennas_score
-from zero_cost_metrics.naswot_layers import get_naswot_layerwise, get_naswot_perturbation_layerwise
+from zero_cost_metrics.naswot_layers import get_naswot_layerwise, get_naswot_perturbation_layerwise, get_naswot_mats_cache_layerwise
 ZC_COLLECTIONS=[
     'grad_norm',
     'snip',
@@ -20,6 +20,7 @@ ZC_COLLECTIONS=[
     'nwot_ssnr',
     'nwot_ptb',
     'nwot_ptb_ssnr',
+    'accum_nwot_ssnr',
 ]
 
 def score_network(metric, model,train_queue,n_classes, device):
@@ -77,6 +78,27 @@ def score_network(metric, model,train_queue,n_classes, device):
     elif metric == 'nwot_ptb_ssnr':
         input, target = next(iter(train_queue))
         nwots, accum_mats_logdet, accum_mats_logdetimp = get_naswot_perturbation_layerwise(model, input, target, device)
+        measures = predictive.find_measures(model,
+                                train_queue,
+                                ('random', 1, n_classes), 
+                                device,
+                                measure_names=['synflow'], aggregate=False)
+        measures = measures['synflow']
+        #return measures
+        scores = []
+        for i in range(len(measures)):
+            s = measures[i].detach().view(-1) 
+            if torch.std(s) == 0:
+                s = torch.sum(s)
+            else:
+                s = torch.sum(s)/torch.std(s)
+            s = s * accum_mats_logdetimp[i]
+
+            scores.append(s.cpu().numpy())
+        return np.sum(scores)
+    elif metric == 'accum_nwot_ssnr':
+        input, target = next(iter(train_queue))
+        K_mats, K_mats_logdet, K_accum_mats, K_accum_mats_logdet, accum_mats_logdetimp = get_naswot_mats_cache_layerwise(model, input, target, device)
         measures = predictive.find_measures(model,
                                 train_queue,
                                 ('random', 1, n_classes), 
