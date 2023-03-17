b69a61859cdd4bc05ce7eb6a1349ef04c9c635a1
MH.Xu <Minghao.Xu.1@warwick.ac.uk>
2023-03-17 08:23:25 +0000

    add v2 of more neat version of nwot mats save method

--- zero_cost_metrics/naswot_layers.py
+++ zero_cost_metrics/naswot_layers.py
@@ -201,3 +201,75 @@ def get_naswot_mats_cache_layerwise(model, input, target, device) -> tuple[list,
     # return scores, accum_mats, accum_mats_logdet
     # return scores, None, accum_mats_logdet, accum_mats_logdet_imp
     return K_mats, K_mats_logdet, K_accum_mats, K_accum_mats_logdet, scores
+
+
+def get_naswot_mats_cache_layerwise_v2(model, input, device) -> tuple[list, list, list, list, list]:
+    '''
+    get and store all cached intermediate mats:
+        - `naswot layerwise mats` (per layer per mat)
+        - - `naswot original` BIG matrix, element-wise sum all `naswot layerwise mats`
+        # - - `naswot accumulated` matrix list, accumulate `naswot layerwise mats` layerly,  ## in v2, we remove this useless mats
+    '''
+    model = copy.deepcopy(model)
+    model.eval()
+    batch_size = input.shape[0]  # TODO: we won't guarantee this first element is batchsize, since it would change in NASBENCH101-micro
+    # #model.K = torch.zeros(batch_size, batch_size).cuda()
+    # model.K_dict = {}  # dict of **naswot matrix logdet**, layer-wise | <k,v>(<model_name,layer_logdet>
+    zeros_mat = torch.zeros(batch_size, batch_size).to(device=device)
+    K_layer_names = [] # list of registered layer (module) names.
+    K_mats = [] # list of **naswot matrix**, layer-wise | [e]([mat, ...])
+    K_mats_logdet = [] # list of **naswot matrix**, layer-wise | [e]([mat, ...])
+    K_accum_mats = [] # list of **accumulated naswot matrix**, layer-wise | [e]([accum_matrix, ...])
+    K_accum_mats_logdet = [] # list of **logdet of accumulated naswot matrix**, layer-wise | [e]([accum_matrix_logdet, ...])
+
+    def counting_forward_hook(module, inp, out):
+        # try:
+        out = out.view(out.size(0), -1)
+        x = (out > 0).float()
+        K = x @ x.t()
+        K2 = (1.-x) @ (1.-x.t())
+        matrix = K + K2
+
+        K_layer_names.append(module.alias)
+        K_mats.append(matrix)
+        K_mats_logdet.append(safe_hooklogdet(K_mats[-1].cpu().numpy()))
+        K_accum_mats.append(matrix if (len(K_accum_mats) == 0) else (K_accum_mats[-1]+matrix))
+        K_accum_mats_logdet.append(safe_hooklogdet(K_accum_mats[-1].cpu().numpy()))
+        # except Exception:
+        #     print('counting_forward_hook ... error')
+        #     print(traceback.format_exc())
+
+    # register forward hook fn
+    registered_layers = []
+    for name, module in model.named_modules():
+        if isinstance(module, nn.Conv2d) or isinstance(module, nn.Linear) or isinstance(module, nn.Conv1d):
+            module.alias = name
+            module.register_forward_hook(counting_forward_hook)
+            registered_layers.append(name)
+    
+    # input = input.cuda(device=device)
+    input = input.to(device=device)
+    with torch.no_grad():
+        model(input)
+    scores = []
+
+    # for name, module in model.named_modules():
+    #     if isinstance(module, nn.Conv2d) or isinstance(module, nn.Linear) or isinstance(module, nn.Conv1d):
+    #         scores.append(model.K_dict[name])
+    assert registered_layers == K_layer_names, 'Not all module forward hook fn were triggered successfully'
+    #scores = copy.deepcopy(scores)
+    
+    # magical calc here||||
+    # a_ori = K_accum_mats_logdet.copy() 
+    a_before = K_accum_mats_logdet[:-1]
+    a_after = K_accum_mats_logdet[1:]
+    a_diff = [i-j for (i,j) in zip(a_after, a_before)] # calc subtraction between neighbour elements.
+    # a_diff.insert(0, 0) # ensure all models' first layer has the same default importance of 0.
+    a_diff.insert(0, K_accum_mats_logdet[0]) # ensure all models' first layer has the same default importance of 0.
+    scores = a_diff
+    
+    del model
+    del input
+    # return scores, accum_mats, accum_mats_logdet
+    # return scores, None, accum_mats_logdet, accum_mats_logdet_imp
+    return K_mats, K_mats_logdet, K_accum_mats, K_accum_mats_logdet, scores
